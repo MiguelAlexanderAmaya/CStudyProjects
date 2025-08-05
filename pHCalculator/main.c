001bcb2c53d910fc70698be9f6768c70c224f8fc
MiguelAlexanderAmaya <amayamiguel528@outlook.com>
2025-08-04 22:21:28 -0600

    New project added, and its description

---
+++ pHCalculator/main.c
@@ -0,0 +1,7 @@
+/******************************/
+/*  pHCalculator              */
+/*  Calculator to get pH      */
+/*  after requesting the user */
+/*  to give either H3O+ or    */
+/*  OH- concentration.        */                 
+/******************************/
